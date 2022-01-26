4349ce60784eb5890286234b6a58c2f98436ee47
Bakhtiyar İ. Amrahov <bakhtiyaria@code.local>
2022-01-26 09:53:11 +0400

    num to n

--- Division/assets/js/main.js
+++ Division/assets/js/main.js
@@ -2,7 +2,7 @@ let num=22 ;
 
 function division(n) {
 
-    if(num%3==0 && num%7==0){
+    if(n%3==0 && n%7==0){
         console.log('Number is divided by 3 and 7');
     }else{
         console.log('Number is not divided by 3 and 7');
